741a713078dd718373dd0d555495aed846f2f3a3
Bart Kessels <development@bk-mail.com>
2024-11-14 17:45:10 +0100

    Implement get year

--- src/domain/repositories/date.repository.ts
+++ src/domain/repositories/date.repository.ts
@@ -1,5 +1,7 @@
 import { Month } from "../models/month";
+import {Year} from 'src/domain/models/year';
 
 export interface DateRepository {
+    getYear(year: number): Year;
     getMonth(year: number, month: number): Month;
 }
