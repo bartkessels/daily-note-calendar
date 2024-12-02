94910610d6561898f68d7c739705ee3136d0b4a2
Bart Kessels <development@bk-mail.com>
2024-12-02 17:44:06 +0100

    Update CSS styles

--- src/components/notes.component.tsx
+++ src/components/notes.component.tsx
@@ -9,18 +9,20 @@ export const NotesComponent = () => {
     const refreshNotesEvent = getRefreshNotesEvent();
 
     refreshNotesEvent?.onEvent('NotesComponent', (notes: Note[]) => {
-       setNotes(notes);
+        setNotes(notes);
     });
 
     return (
-        <ul>
-            {notes.map((note: Note) => (
-                <li key={note.path} title={note.path} onClick={() => noteEvent?.emitEvent(note)}>
-                    <span className="note-title">{note.name}</span><br/>
-                    <span className="note-date">Created at {note.createdOn.toLocaleTimeString()}</span><br/>
-                    <span className="note-path">{note.path}</span>
-                </li>
-            ))}
-        </ul>
+        <div className="dnc">
+            <ul>
+                {notes.map((note: Note) => (
+                    <li key={note.path} title={note.path} onClick={() => noteEvent?.emitEvent(note)}>
+                        <span className="note-title">{note.name}</span><br/>
+                        <span className="note-date">Created at {note.createdOn.toLocaleTimeString()}</span><br/>
+                        <span className="note-path">{note.path}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
