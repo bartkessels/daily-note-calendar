04362781f37a1abc111d80d81d21adf9c6b94f7b
Bart Kessels <development@bk-mail.com>
2024-12-05 14:19:15 +0100

    Make the PeriodNNoteEvent a bit less generic

--- src/implementation/events/periodic-note.event.ts
+++ src/implementation/events/periodic-note.event.ts
@@ -1,3 +1,4 @@
 import {Event} from 'src/domain/events/event';
+import {Period} from 'src/domain/models/period';
 
-export class PeriodicNoteEvent<T> extends Event<T> {}
+export class PeriodicNoteEvent<T extends Period<any>> extends Event<T> {}
